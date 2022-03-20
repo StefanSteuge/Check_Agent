a9db35e5e0d816f02cc1c24c892fd074f2c8fad4
Stepan Dulka <fotosteuge@gmail.com>
2022-03-20 16:18:07 +0200

    Commit code formatting

--- src/com/cursor/Main.java
+++ src/com/cursor/Main.java
@@ -3,31 +3,22 @@ package com.cursor;
 import java.util.Scanner;
 
 public class Main {
-    final static int PASSWORD = 133976; //You can change pass, but don't change the type
+    final static int PASSWORD = 133976;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         checkPassword(scanner.nextInt());
     }
 
-    public static void checkPassword(int pass){
+    public static void checkPassword(int pass) {
         if (pass == PASSWORD) {
             System.out.println("Hello, agent");
-        }else {
+        } else {
             System.out.println("Access Denied");
         }
     }
-
-
-
-
 }
 
 
-/* - запрашивает на вход число-пароль
-- если оно равно секретному числу, программа выводит: "Hello, agent"
-- в противном случае печатает: "Access Denied".
-
- */
 
 
